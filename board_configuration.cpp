826c31175b8ace32bba4fc569eff1a19237cb98c
eeklaca <laszlo.toth@etaloninfo.hu>
2025-07-08 12:57:01 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -1,5 +1,78 @@
 #include "pch.h"
 
+static void setInjectorPins() {
+	engineConfiguration->injectionPins[0] = Gpio::B15;
+	engineConfiguration->injectionPins[1] = Gpio::B14;
+	engineConfiguration->injectionPins[2] = Gpio::B12;
+	engineConfiguration->injectionPins[3] = Gpio::B13;
+	engineConfiguration->injectionPins[4] = Gpio::A8;
+	engineConfiguration->injectionPins[5] = Gpio::E7;
+	engineConfiguration->injectionPins[6] = Gpio::E13;
+	engineConfiguration->injectionPins[7] = Gpio::E10;
+}
+
+static void setIgnitionPins() {
+	engineConfiguration->ignitionPins[0] = Gpio::E2;
+	engineConfiguration->ignitionPins[1] = Gpio::E3;
+	engineConfiguration->ignitionPins[2] = Gpio::C13;
+	engineConfiguration->ignitionPins[3] = Gpio::E6;
+	engineConfiguration->ignitionPins[4] = Gpio::E4;
+	engineConfiguration->ignitionPins[5] = Gpio::E5;
+	engineConfiguration->ignitionPins[6] = Gpio::E0;
+	engineConfiguration->ignitionPins[7] = Gpio::B9;
+}
+
+static void setEtbConfig() {
+	// VNH7070 driver
+	// This chip has four control pins:
+	// DIR1 - open direction of the motor
+	// DIR2 - open direction of the motor
+	// PWM - pwm control (enable high, coast low)
+	// DIS - disables motor (enable low)
+
+	// Throttle #1
+	// PWM pin
+	engineConfiguration->etbIo[0].controlPin = Gpio::B11;
+	// DIR1 pin
+	engineConfiguration->etbIo[0].directionPin1 = Gpio::E15;
+	// DIR2 pin
+	engineConfiguration->etbIo[0].directionPin2 = Gpio::B10;
+	// Disable pin
+	engineConfiguration->etbIo[0].disablePin = Gpio::C9;
+
+	// Throttle #2
+	// PWM pin
+	engineConfiguration->etbIo[1].controlPin = Gpio::Unassigned;
+	// DIR1 pin
+	engineConfiguration->etbIo[1].directionPin1 = Gpio::Unassigned;
+	// DIR2 pin
+	engineConfiguration->etbIo[1].directionPin2 = Gpio::Unassigned;
+	// Disable pin
+	engineConfiguration->etbIo[1].disablePin = Gpio::Unassigned;
+
+	// we only have pwm/dir, no dira/dirb
+	engineConfiguration->etb_use_two_wires = false;
+}
+
+static void setupVbatt() {
+	// 910R high side/470R low side = 1.54 ratio divider
+	engineConfiguration->analogInputDividerCoefficient = 1.54f;
+	
+	// 3.9k high side/ 1k low side
+	engineConfiguration->vbattDividerCoeff = (7.15 + 1) / 1; 
+
+	// Battery sense on PA4
+	engineConfiguration->vbattAdcChannel = EFI_ADC_4;
+
+	engineConfiguration->adcVcc = 3.3f;
+}
+
+static void setStepperConfig() {
+	engineConfiguration->idle.stepperDirectionPin = Gpio::Unassigned;
+	engineConfiguration->idle.stepperStepPin = Gpio::Unassigned;
+	engineConfiguration->stepperEnablePin = Gpio::Unassigned;
+}
+
 Gpio getCommsLedPin() {
 	return Gpio::Unassigned;
 }
@@ -12,33 +85,36 @@ Gpio getWarningLedPin() {
 	return Gpio::Unassigned;
 }
 
-// board-specific configuration setup
-void setBoardDefaultConfiguration() {
-    // engineConfiguration->injectionPins[0] = Gpio::F13;
-    // engineConfiguration->ignitionPins[0] = Gpio::E15;
-
-//   engineConfiguration->triggerInputPins[0] = Gpio::B1;
-//	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
-
-//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
-
-//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
-
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
-
-
-    	// 5.6k high side/10k low side = 1.56 ratio divider
-  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
-
-    	// 6.34k high side/ 1k low side
-//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
-
-//	engineConfiguration->adcVcc = 3.3f;
+void setBoardConfigOverrides() {
+	setupVbatt();
+	setEtbConfig();
+	setStepperConfig();
 
 	engineConfiguration->clt.config.bias_resistor = 2490;
 	engineConfiguration->iat.config.bias_resistor = 2490;
 
+	//CAN 1 bus overwrites
+	engineConfiguration->canRxPin = Gpio::D0;
+	engineConfiguration->canTxPin = Gpio::D1;
 
-	// Battery sense on PA0
-//	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
+	//CAN 2 bus overwrites
+	engineConfiguration->can2RxPin = Gpio::Unassigned;
+	engineConfiguration->can2TxPin = Gpio::Unassigned;
+}
+
+// board-specific configuration setup
+void setBoardDefaultConfiguration(void) {
+	setInjectorPins();
+	setIgnitionPins();
+
+	engineConfiguration->isSdCardEnabled = true;
+
+	engineConfiguration->triggerInputPins[0] = Gpio::D3;
+	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
+	// MAP on PA3
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
+	// CLT on PA1
+	engineConfiguration->clt.adcChannel = EFI_ADC_1;
+	// IAT on PA0
+	engineConfiguration->iat.adcChannel = EFI_ADC_0;
 }
