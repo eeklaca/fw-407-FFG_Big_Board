97075c32ad1dce8452cf27bcc66a7ad3105ee268
eeklaca <laszlo.toth@etaloninfo.hu>
2025-07-10 12:29:11 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -1,28 +1,28 @@
 #include "pch.h"
 
-//static void setInjectorPins() {
-//	engineConfiguration->injectionPins[0] = Gpio::B15;
-//	engineConfiguration->injectionPins[1] = Gpio::B14;
-//	engineConfiguration->injectionPins[2] = Gpio::B12;
-//	engineConfiguration->injectionPins[3] = Gpio::B13;
-//	engineConfiguration->injectionPins[4] = Gpio::A8;
-//	engineConfiguration->injectionPins[5] = Gpio::E7;
-//	engineConfiguration->injectionPins[6] = Gpio::E13;
-//	engineConfiguration->injectionPins[7] = Gpio::E10;
-//}
+static void setInjectorPins() {
+	engineConfiguration->injectionPins[0] = Gpio::B15;
+	engineConfiguration->injectionPins[1] = Gpio::B14;
+	engineConfiguration->injectionPins[2] = Gpio::B12;
+	engineConfiguration->injectionPins[3] = Gpio::B13;
+	engineConfiguration->injectionPins[4] = Gpio::A8;
+	engineConfiguration->injectionPins[5] = Gpio::E7;
+	engineConfiguration->injectionPins[6] = Gpio::E13;
+	engineConfiguration->injectionPins[7] = Gpio::E10;
+}
 
-//static void setIgnitionPins() {
-//	engineConfiguration->ignitionPins[0] = Gpio::E2;
-//	engineConfiguration->ignitionPins[1] = Gpio::E3;
-//	engineConfiguration->ignitionPins[2] = Gpio::C13;
-//	engineConfiguration->ignitionPins[3] = Gpio::E6;
-//	engineConfiguration->ignitionPins[4] = Gpio::E4;
-//	engineConfiguration->ignitionPins[5] = Gpio::E5;
-//	engineConfiguration->ignitionPins[6] = Gpio::E0;
-//	engineConfiguration->ignitionPins[7] = Gpio::B9;
-//}
+static void setIgnitionPins() {
+	engineConfiguration->ignitionPins[0] = Gpio::E2;
+	engineConfiguration->ignitionPins[1] = Gpio::E3;
+	engineConfiguration->ignitionPins[2] = Gpio::C13;
+	engineConfiguration->ignitionPins[3] = Gpio::E6;
+	engineConfiguration->ignitionPins[4] = Gpio::E4;
+	engineConfiguration->ignitionPins[5] = Gpio::E5;
+	engineConfiguration->ignitionPins[6] = Gpio::E0;
+	engineConfiguration->ignitionPins[7] = Gpio::B9;
+}
 
-//static void setEtbConfig() {
+static void setEtbConfig() {
 	// VNH7070 driver
 	// This chip has four control pins:
 	// DIR1 - open direction of the motor
@@ -32,26 +32,26 @@
 
 	// Throttle #1
 	// PWM pin
-//	engineConfiguration->etbIo[0].controlPin = Gpio::B11;
+	engineConfiguration->etbIo[0].controlPin = Gpio::B11;
 	// DIR1 pin
-//	engineConfiguration->etbIo[0].directionPin1 = Gpio::E15;
+	engineConfiguration->etbIo[0].directionPin1 = Gpio::E15;
 	// DIR2 pin
-//	engineConfiguration->etbIo[0].directionPin2 = Gpio::B10;
+	engineConfiguration->etbIo[0].directionPin2 = Gpio::B10;
 	// Disable pin
-//	engineConfiguration->etbIo[0].disablePin = Gpio::C9;
+	engineConfiguration->etbIo[0].disablePin = Gpio::C9;
 
 	// Throttle #2
 	// PWM pin
-//	engineConfiguration->etbIo[1].controlPin = Gpio::Unassigned;
+	engineConfiguration->etbIo[1].controlPin = Gpio::Unassigned;
 	// DIR1 pin
-//	engineConfiguration->etbIo[1].directionPin1 = Gpio::Unassigned;
+	engineConfiguration->etbIo[1].directionPin1 = Gpio::Unassigned;
 	// DIR2 pin
-//	engineConfiguration->etbIo[1].directionPin2 = Gpio::Unassigned;
+	engineConfiguration->etbIo[1].directionPin2 = Gpio::Unassigned;
 	// Disable pin
-//	engineConfiguration->etbIo[1].disablePin = Gpio::Unassigned;
+	engineConfiguration->etbIo[1].disablePin = Gpio::Unassigned;
 
 	// we only have pwm/dir, no dira/dirb
-//	engineConfiguration->etb_use_two_wires = false;
+	engineConfiguration->etb_use_two_wires = false;
 }
 
 static void setupVbatt() {
@@ -87,8 +87,6 @@ Gpio getWarningLedPin() {
 
 void setBoardConfigOverrides() {
 	setupVbatt();
-	setEtbConfig();
-	setStepperConfig();
 
 	engineConfiguration->clt.config.bias_resistor = 2490;
 	engineConfiguration->iat.config.bias_resistor = 2490;
@@ -106,6 +104,8 @@ void setBoardConfigOverrides() {
 void setBoardDefaultConfiguration(void) {
 	setInjectorPins();
 	setIgnitionPins();
+	setEtbConfig();
+	setStepperConfig();
 
 	engineConfiguration->isSdCardEnabled = true;
 
@@ -117,4 +117,20 @@ void setBoardDefaultConfiguration(void) {
 	engineConfiguration->clt.adcChannel = EFI_ADC_1;
 	// IAT on PA0
 	engineConfiguration->iat.adcChannel = EFI_ADC_0;
+
+	engineConfiguration->spi1mosiPin = Gpio::B5;
+	engineConfiguration->spi1misoPin = Gpio::B4;
+	engineConfiguration->spi1sckPin = Gpio::B3;
+
+	engineConfiguration->spi2mosiPin = Gpio::Unassigned;
+	engineConfiguration->spi2misoPin = Gpio::Unassigned;
+	engineConfiguration->spi2sckPin = Gpio::Unassigned;
+
+	engineConfiguration->spi3mosiPin = Gpio::C12;
+	engineConfiguration->spi3misoPin = Gpio::C11;
+	engineConfiguration->spi3sckPin = Gpio::C10;
+
+	engineConfiguration->is_enabled_spi_1 = true;
+	engineConfiguration->is_enabled_spi_2 = false;
+	engineConfiguration->is_enabled_spi_3 = true;
 }
