bdb9b7d1c9cf2ef942935d044b1a1eb8da44b7c5
GitHub git update Action <action@github.com>
2025-09-22 02:00:03 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_ffgbigboard.h
+++ generated/controllers/generated/rusefi_generated_ffgbigboard.h
@@ -2076,7 +2076,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.09.21.ffgbigboard.1754203721"
+#define TS_SIGNATURE "rusEFI main.2025.09.22.ffgbigboard.1754203721"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
