ea8c9d838f47e00ad3a049c2030b97b1a991be3a
GitHub git update Action <action@github.com>
2025-09-30 19:46:06 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_ffgbigboard.h
+++ generated/controllers/generated/signature_ffgbigboard.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3398042113
-#define TS_SIGNATURE "rusEFI main.2025.09.30.ffgbigboard.3398042113"
+#define SIGNATURE_HASH 3724050894
+#define TS_SIGNATURE "rusEFI main.2025.09.30.ffgbigboard.3724050894"
