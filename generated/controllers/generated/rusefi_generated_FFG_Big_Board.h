2b6fa65cb490afc494493be7a5643e949a06ff39
GitHub git update Action <action@github.com>
2026-08-21 01:02:57 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_FFG_Big_Board.h
+++ generated/controllers/generated/rusefi_generated_FFG_Big_Board.h
@@ -2287,7 +2287,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.08.20.FFG_Big_Board.3582487244"
+#define TS_SIGNATURE "rusEFI main.2026.08.21.FFG_Big_Board.3582487244"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
